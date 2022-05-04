37746bf63dd58db8050f0c81409a22bad4cefacf
ycshang <351234359@qq.com>
2022-05-04 18:55:30 +0800

    feat: 初始化项目结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    quotes: "off",
-    'space-before-function-paren': 'off'
+    'space-before-function-paren': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'warn'
   }
 }
