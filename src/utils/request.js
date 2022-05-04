d1f3d173c74715464d658431222590e652f11fab
ycshang <351234359@qq.com>
2022-05-04 20:46:13 +0800

    feat: 用户被动退出方案之被动处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -23,6 +23,11 @@ service.interceptors.response.use(
     }
   },
   (error) => {
+    if (
+      error.response && error.response.data && error.response.data.code === 401
+    ) {
+      store.dispatch('user/logout')
+    }
     // TODO: 将来处理 token 超时问题
     ElMessage.error(error.message) // 提示错误信息
     return Promise.reject(error)
