a97ed1e351b3c38048c741e7178937ea6fb2d2eb
ycshang <351234359@qq.com>
2022-05-04 20:15:42 +0800

    feat: 创建基于Layout的基础架构

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,39 @@
 <template>
-  <div class="">Layout 页面</div>
+  <div class="app-wrapper">
+    <!-- 左侧 menu -->
+    <sidebar class="sidebar-container" :style="{ backgroundColor: variables.menuBg }" />
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部的 navbar -->
+        <navbar />
+      </div>
+      <!-- 内容区 -->
+      <AppMain />
+    </div>
+  </div>
 </template>
-
 <script setup>
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import AppMain from './components/AppMain'
 import { } from 'vue'
+import variables from '@/style/variables.module.scss'
 </script>
+<style lang="scss" scoped>
+@import '../style/mixins.scss';
+@import '../style/variables.module.scss';
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
 
-<style lang="scss" scoped></style>
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
