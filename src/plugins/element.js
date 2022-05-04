104447dea345470dca0260579de40164ce1cde40
ycshang <351234359@qq.com>
2022-05-04 23:59:50 +0800

    feat: 自定义语言包国际化处理

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,11 @@
+import store from '@/store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/lib/locale/lang/en'
 
 export default (app) => {
-  app.use(ElementPlus)
+  app.use(ElementPlus, {
+    locale: store.getters.language === 'en' ? en : zhCn
+  })
 }
