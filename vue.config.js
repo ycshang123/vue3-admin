749929284c141220d05916d5e5a21b2d86d23dbb
ycshang <351234359@qq.com>
2022-05-04 19:44:48 +0800

    feat: 登录触发动作

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
-function resolve(dir) {
+function resolve (dir) {
   return path.join(__dirname, dir)
 }
 // https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
 module.exports = defineConfig({
   transpileDependencies: true,
-  chainWebpack(config) {
+  chainWebpack (config) {
     // 设置 svg-sprite-loader
     config.module.rule('svg').exclude.add(resolve('src/icons')).end()
     config.module
@@ -20,5 +20,17 @@ module.exports = defineConfig({
         symbolId: 'icon-[name]'
       })
       .end()
+  },
+  devServer: {
+    // 配置反向代理
+    proxy: {
+      // 当地址中有/api的时候会触发代理机制
+      '/api': {
+        // 要代理的服务器地址
+        target: 'http://127.0.0.1:4523/mock/934666/',
+        // 是否跨域
+        changeOrigin: true
+      }
+    }
   }
 })
