0b398fc80d7a0828163f2df2517147dcd1bcaf22
ycshang <351234359@qq.com>
2022-05-04 19:01:54 +0800

    feat: 美化登录页面样式

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
-
+// 导入全局样式
+import './style/index.scss'
 const app = createApp(App)
 installElementPlus(app)
 app.use(store).use(router).mount('#app')
