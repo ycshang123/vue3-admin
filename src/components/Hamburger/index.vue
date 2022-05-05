4daecb8dbd7e1b8d29222a09313d0366d0cb7846
ycshang <351234359@qq.com>
2022-05-06 00:08:49 +0800

    fix: 修复HeaderSearch的关闭和国际化问题

--- src/components/Hamburger/index.vue
+++ src/components/Hamburger/index.vue
@@ -1,126 +1,31 @@
 <template>
-  <div :class="{ show: isShow }" class="header-search">
-    <svg-icon class-name="search-icon" icon="search" @click.stop="onShowClick" />
-    <el-select
-      ref="headerSearchSelectRef"
-      class="header-search-select"
-      v-model="search"
-      filterable
-      default-first-option
-      remote
-      placeholder="Search"
-      :remote-method="querySearch"
-      @change="onSelectChange"
-    >
-      <el-option
-        v-for="option in searchOptions"
-        :key="option.item.path"
-        :label="option.item.title.join(' > ')"
-        :value="option.item"
-      ></el-option>
-    </el-select>
+  <div class="hamburger-container" @click="toggleClick">
+    <svg-icon class="hamburger" :icon="icon"></svg-icon>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
-import { filterRouters } from '@/utils/route'
-import { useRouter } from 'vue-router'
-import { generateRoutes } from '../HeaderSearch/FuseData'
-import Fuse from 'fuse.js'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 
-// 控制 search 显示
-const isShow = ref(false)
-// el-select 实例
-const headerSearchSelectRef = ref(null)
-const onShowClick = () => {
-  isShow.value = !isShow.value
-  headerSearchSelectRef.value.focus()
-}
-// 搜索结果
-const searchOptions = ref([])
-
-// search 相关
-const search = ref('')
-// 搜索方法
-const querySearch = (query) => {
-  // console.log(fuse.search(query))
-  if (query !== '') {
-    searchOptions.value = fuse.search(query)
-  } else {
-    searchOptions.value = []
-  }
-}
-// 选中回调
-const onSelectChange = (val) => {
-  router.push(val.path)
-  onClose()
+const store = useStore()
+const toggleClick = () => {
+  store.commit('app/triggerSidebarOpened')
 }
 
-// 检索数据源
-const router = useRouter()
-const searchPool = computed(() => {
-  const filterRoutes = filterRouters(router.getRoutes())
-  return generateRoutes(filterRoutes)
-})
-
-/**
- * 搜索库相关
- */
-const fuse = new Fuse(searchPool.value, {
-  // 是否按优先级进行排序
-  shouldSort: true,
-  // 匹配长度超过这个值的才会被认为是匹配的
-  minMatchCharLength: 1,
-  // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
-  // name：搜索的键
-  // weight：对应的权重
-  keys: [
-    {
-      name: 'title',
-      weight: 0.7
-    },
-    {
-      name: 'path',
-      weight: 0.3
-    }
-  ]
-})
-console.log(searchPool)
+const icon = computed(() =>
+  store.getters.sidebarOpened ? 'hamburger-opened' : 'hamburger-closed'
+)
 </script>
 
 <style lang="scss" scoped>
-.header-search {
-  font-size: 0 !important;
-  .search-icon {
-    cursor: pointer;
-    font-size: 18px;
-    vertical-align: middle;
-  }
-  .header-search-select {
-    font-size: 18px;
-    transition: width 0.2s;
-    width: 0;
-    overflow: hidden;
-    background: transparent;
-    border-radius: 0;
+.hamburger-container {
+  padding: 0 16px;
+  .hamburger {
     display: inline-block;
     vertical-align: middle;
-
-    ::v-deep(.el-input__inner) {
-      border-radius: 0;
-      border: 0;
-      padding-left: 0;
-      padding-right: 0;
-      box-shadow: none !important;
-      vertical-align: middle;
-    }
-  }
-  &.show {
-    .header-search-select {
-      width: 210px;
-      margin-left: 10px;
-    }
+    width: 20px;
+    height: 20px;
   }
 }
 </style>
