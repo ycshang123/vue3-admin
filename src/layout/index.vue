268dfc90ab5d3243b0f9e3aaaaaea02f2a12c8f1
ycshang <351234359@qq.com>
2022-05-05 14:44:38 +0800

    feat: 自定义主题变更

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-wrapper" :class="[$store.getters.sidebarOpened ? 'openSidebar' : 'hideSidebar']">
     <!-- 左侧 menu -->
-    <sidebar class="sidebar-container" :style="{ backgroundColor: variables.menuBg }" />
+    <sidebar id="guide-sidebar" class="sidebar-container" :style="{ backgroundColor: $store.getters.cssVar.menuBg }" />
     <div class="main-container">
       <div class="fixed-header">
         <!-- 顶部的 navbar -->
@@ -17,7 +17,6 @@ import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import AppMain from './components/AppMain'
 import { } from 'vue'
-import variables from '@/style/variables.module.scss'
 </script>
 <style lang="scss" scoped>
 @import '../style/mixins.scss';
