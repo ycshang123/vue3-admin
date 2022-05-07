660885766ce757aacb25f80e92693a5595f91b21
ycshang <351234359@qq.com>
2022-05-07 23:34:36 +0800

    feat: 辅助业务之用户删除

--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -37,7 +37,7 @@
           <template #default>
             <el-button type="primary" @click="onImportExcelClick"> {{ $t('msg.excel.importExcel') }}</el-button>
             <el-button type="info" size="mini">{{ $t('msg.excel.showRole') }}</el-button>
-            <el-button type="danger" size="mini">{{ $t('msg.excel.remove') }}</el-button>
+            <el-button type="danger" size="mini" @click="onRemoveClick(row)">{{ $t('msg.excel.remove') }}</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -58,10 +58,10 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { getUserManageList } from '@/api/user-manage'
 import { watchSwitchLang } from '@/utils/i18n'
+import { ref, onActivated } from 'vue'
 
 // 数据相关
 const tableData = ref([])
@@ -100,6 +100,27 @@ const handleCurrentChange = (currentPage) => {
 const router = useRouter()
 const onImportExcelClick = () => {
   router.push('/user/import')
+}
+// 处理导入用户后数据不重新加载的问题
+onActivated(getListData)
+/**
+ * 删除按钮点击事件
+ */
+const i18n = useI18n()
+const onRemoveClick = row => {
+  ElMessageBox.confirm(
+    i18n.t('msg.excel.dialogTitle1') +
+    row.username +
+    i18n.t('msg.excel.dialogTitle2'),
+    {
+      type: 'warning'
+    }
+  ).then(async () => {
+    await deleteUser(row._id)
+    ElMessage.success(i18n.t('msg.excel.removeSuccess'))
+    // 重新渲染数据
+    getListData()
+  })
 }
 </script>
 
