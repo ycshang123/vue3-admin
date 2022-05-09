66b8f9ce9dad65d81b734e47a338cf3d5ba11e62
ycshang <351234359@qq.com>
2022-05-09 14:24:18 +0800

    feat: markdown实现

---
+++ src/views/article-create/components/Markdown.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="markdown-container">
+    <!-- 渲染区 -->
+    <div id="markdown-box"></div>
+    <div class="bottom">
+      <el-button type="primary" @click="onSubmitClick">{{ $t('msg.article.commit') }}</el-button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.markdown-container {
+  .bottom {
+    margin-top: 20px;
+    text-align: right;
+  }
+}
+</style>
+<script setup>
+import MkEditor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import '@toast-ui/editor/dist/i18n/zh-cn'
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+
+// Editor实例
+let mkEditor
+// 处理离开页面切换语言导致 dom 无法被获取
+let el
+onMounted(() => {
+  el = document.querySelector('#markdown-box')
+  initEditor()
+})
+
+const store = useStore()
+const initEditor = () => {
+  mkEditor = new MkEditor({
+    el,
+    height: '500px',
+    previewStyle: 'vertical',
+    language: store.getters.language === 'zh' ? 'zh-CN' : 'en'
+  })
+
+  mkEditor.getMarkdown()
+}
+</script>
